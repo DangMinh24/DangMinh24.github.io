42a4f66dca28b687199a01180ad233ecd4af9ef1
Dang Tran <dangtm24@gmail.com>
2022-10-24 09:09:11 -0500

    Use bootstrap nav instead

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,74 +1,56 @@
 <template>
-  <div>
-    <ul class="nav nav-pills">
-      <li class="nav-item">
-        <router-link
-          to="/"
-          class="nav-link"
-          active-class="active"
-          exact
-          style="cursor: pointer"
-          >Home</router-link
-        >
-      </li>
-      <li class="nav-item">
-        <router-link
-          to="/research"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >Research</router-link
-        >
-      </li>
-      <li class="nav-item">
-        <router-link
-          to="/education"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >Education</router-link
-        >
-      </li>
-      <li class="nav-item">
-        <router-link
-          to="/experience"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >Experience</router-link
-        >
-      </li>
-
-      <li class="nav-item">
-        <router-link
-          to="/cv"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >CV</router-link
-        >
-      </li>
-      <li class="nav-item">
-        <router-link
-          to="/blog"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >Blog</router-link
-        >
-      </li>
-      <li class="nav-item">
-        <router-link
-          to="/contact"
-          class="nav-link"
-          active-class="active"
-          style="cursor: pointer"
-          >Contact</router-link
-        >
-      </li>
-    </ul>
-    <hr />
-  </div>
+  <nav class="nav">
+    <router-link
+      to="/"
+      class="nav-link"
+      active-class="active"
+      exact
+      style="cursor: pointer"
+      >Home</router-link
+    >
+    <router-link
+      to="/research"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >Research</router-link
+    >
+    <router-link
+      to="/education"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >Education</router-link
+    >
+    <router-link
+      to="/experience"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >Experience</router-link
+    >
+    <router-link
+      to="/cv"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >CV</router-link
+    >
+    <router-link
+      to="/blog"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >Blog</router-link
+    >
+    <router-link
+      to="/contact"
+      class="nav-link"
+      active-class="active"
+      style="cursor: pointer"
+      >Contact</router-link
+    >
+  </nav>
 </template>
 
 <script>
